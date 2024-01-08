e0f8bf1f634a299e695aa0ff2c6be87fd6067504
= <=>
2024-01-08 19:13:50 +0530

    deployment bug 3 fix

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -7,7 +7,8 @@ const authOptions={
             clientId:process.env.GITHUB_CLIENT_ID,
             clientSecret:process.env.GITHUB_CLIENT_SECRET
         })
-    ]
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
 }
 const handler = NextAuth(authOptions);
 export {handler as GET , handler as POST}
